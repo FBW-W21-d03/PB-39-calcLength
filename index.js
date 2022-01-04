d97a3dc8577db170ced1438ddd443561dd353089
Hayat Alkheder <hayat.alkheder@gmail.com>
2022-01-04 10:13:50 +0100

    js

---
+++ index.js
@@ -0,0 +1,10 @@
+let text = "Hello World!";
+//let length = text.length;
+
+
+function calcLength(name){
+    console.log("Der Name ist folgende Zeichen lang: ",name.length);
+}
+
+calcLength("John");
+calcLength("Bob");
