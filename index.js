eab869820c8322d371d2cde9962b8a935a123aa5
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2022-01-04 10:04:56 +0100

    JS calcLength

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+console.log("-------------------------");
+console.log("javascript-calcLength");
+//aufgabe teil 1
+function calcLength(name){
+    console.log("Der Name ist folgende Zeichen lang: ", name.length)
+}
+
+
+calcLength('John');
+calcLength('Bob');
