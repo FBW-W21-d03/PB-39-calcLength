dcd1de5747b0c239c1cb3c7e483460772c968042
aisha2000-hub <aisha.noori2000@gmail.com>
2022-01-04 10:10:47 +0100

    Die lösung von Aisha

---
+++ index.js
@@ -0,0 +1,10 @@
+// AUFGABE TEIL-1
+function calcLength(name){
+
+    console.log("Der Name ist folgende Zeichen lang: ", name.length)
+
+}
+
+// AUFABE TEIL-2
+calcLength("John");
+calcLength("Bob");
