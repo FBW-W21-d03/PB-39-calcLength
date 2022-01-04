046ff7dce288830acf6475878d8f43695015837e
Mohal1125 <rajwindersinghmohal@yahoo.com>
2022-01-04 15:14:55 +0100

    mein lösung

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function calcLength(name) {
- console.log("Der Name ist folgende Zeichen lang: " + name.length);
+return "Der Name ist folgende Zeichen lang: " + name.length;
 }
-calcLength("john");
-calcLength("Bob");
-calcLength("Rajwinder Singh");
+
+let return1 = calcLength("John");
+let return2 = calcLength("Bob");
+let return3 = calcLength("Rajwinder Singh");
+console.log(return1);
+console.log(return2);
+console.log(return3);
