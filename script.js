a40efee538543b0b052d6361d51b04929c5ad50a
Vens <v.kolev@alfatrex.com>
2022-01-04 15:19:11 +0100

    vens return

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function calcLength (name) {
-    console.log("Der Name ist folgende Zeichen lang: ", name.length)
+    return "Der Name ist folgende Zeichen lang: ", name.length
 };
 
 calcLength ("John");
 calcLength("Bob");
+
+// Returns
+
+
+let returnet = calcLength("John");
+let returnet2 = calcLength("Bob");
+let returnet3 = calcLength("Chuk");
+
+console.log(returnet);
+console.log(returnet2);
+console.log(returnet3);
