21e0e654375a8f143aeb6e424aaa749fae54e9a5
Dennisbullys <d.rochus@hotmail.de>
2022-01-04 14:55:19 +0100

    Dennis

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-// Aufgabe 1
-
+/*
+Neue Aufgabe mit Return Function 
+*/
 function calcLength(name){
     
-    console.log ("Der Name ist folgende Zeichen lang: ",name.length)
-}
+    return name ,"Der Name ist folgende Zeichen lang: ",name.length;
+};
+
+let result = calcLength("John");
+
+let result2 = calcLength("Bob");
+
+let result3 = calcLength("Dennis");
+
+console.log(result);
+console.log(result2);
+console.log(result3);
 
 
-// Aufgabe 2
 
-calcLength("John");
-calcLength("Bob");
-calcLength("Dennis");
