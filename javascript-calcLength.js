46257d1bfc325c4a0a1bde6d95bcfbe0849751ab
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2022-01-04 14:45:55 +0100

    Orhan's lösung2

--- javascript-calcLength.js
+++ javascript-calcLength.js
@@ -1,10 +1,13 @@
 // Aufgaabe 1
 function calcLength(name) {
-    name = name.length;
-    console.log("Der Name ist folgende Zeichen lang: ", name);
+
+    return "Der Name ist folgende Zeichen lang: " + name.length;
     
 }
 // Aufgaabe 2
 
-calcLength("John");
-calcLength("Bob");
+let ergebnis1 = calcLength("John");
+let ergebnis2 = calcLength("Bob");
+
+console.log(ergebnis1);
+console.log(ergebnis2);
