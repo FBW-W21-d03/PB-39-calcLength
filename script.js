94bf2297003348f45209f0a4d2e2bc88904dcc2c
sham912411 <shamalhakim91@gmail.com>
2022-01-04 15:30:42 +0100

    lösung

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function calcLength (name) {
-    console.log("dein Wort hat sovielen Zeichen" + name.length)
-    }
-    calcLength("John");
-    calcLength("Angela Merkel");
-
-
+function calcLength(name) {
+    console.log(
+      'Der Name "' + name + '" ist folgende Zeichen lang:',
+      name.length
+    );
+  }
   
-    return "Der Name ist folgende Zeichen lang: " + name.length;
+  calcLength("John");
+  calcLength("Bob");
