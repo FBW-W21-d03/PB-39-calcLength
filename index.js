3181828a0e03b84e570596bfc06f5309f9f9766e
Sharfuddin Ahmed <Sharfuddinahmed07@gmail.com>
2022-01-04 10:20:32 +0100

    lösung

---
+++ index.js
@@ -0,0 +1,13 @@
+
+  
+// Aufgaabe Teil 1
+function calcLength(name) {
+    name = name.length;
+    console.log("Der Name ist folgende Zeichen lang: ", name);
+    
+}
+
+// Aufgaabe Teil 2
+
+calcLength("John");
+calcLength("Bob");
