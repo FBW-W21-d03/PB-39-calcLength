9f7133c9325831201a3a907cda7f9a3679d62d67
kawanaza <kawanbabani@gmail.com>
2022-01-04 10:12:43 +0100

    Done

---
+++ index.js
@@ -0,0 +1,11 @@
+
+//Aufgabenstellung - Teil 1
+
+function calcLength(name) {
+  console.log("Der Name ist  nicht folgende Zeichen lang: ", name.lenght);
+}
+
+//Aufgabenstellung - Teil 2
+calcLength("John");
+calcLength("Bob");
+calcLength("Jo")
