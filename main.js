be49c9425e8d01163a26226b98aaf881d247c549
Dennisbullys <d.rochus@hotmail.de>
2022-01-04 10:08:01 +0100

    Dennis

---
+++ main.js
@@ -0,0 +1,12 @@
+// Aufgabe 1
+
+function calcLength(name){
+    
+    console.log ("Der Name ist folgende Zeichen lang: ",name.length)
+}
+
+
+// Aufgabe 2
+
+calcLength("John");
+calcLength("Bob");
