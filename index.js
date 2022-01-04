fabb6bb1a3c800517f20ee9d24d8744091c9067a
Serdar68 <Serdarbalkis@outlook.de>
2022-01-04 10:13:02 +0100

    serdar

---
+++ index.js
@@ -0,0 +1,19 @@
+// javascript-calcLength
+
+/* Aufgabenstellung - Teil 1
+Schreibe eine Funktion calcLength(name).
+Die Funktion nimmt den Parameter namens "name" auf.
+In der Funktion wird die Länge des Parameters ermittelt.
+Die Funktion gibt die Ausgabe ("Der Name ist folgende Zeichen lang: ", x) in die Konsole aus.*/
+
+function calcLength(name){
+        console.log("Der Name ist folgende Zeichen lang: ", name.length);
+}
+
+
+/* Aufgabenstellung - Teil 2
+Teste deine Funktion.
+Führe sie mit dem Argument "John" aus.
+Führe sie anschließend mit dem Argument "Bob" aus.*/
+calcLength("John")
+calcLength("Bob")
