b30f5fcb68dfc34c6552b11305aa18852f0accec
slavablack <veaceslav.ciornii@magenta.de>
2022-01-04 14:54:47 +0100

    Veaceslav

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function calcLength (name) {
   
-    console.log("Der Name ist folgende Zeichen lang: ", name.length)
+    console.log("Der Name ist folgende Zeichen lang: "+ name.length)
 
 };
 calcLength ("John");
 calcLength("Bob");
+//Return Statement in der Function
+function calcLength (name) {
+  
+    return "Der Name ist folgende Zeichen lang: " + name.length
+
+};
+let lenght= calcLength ("John");
+let lenght2= calcLength ("Bob");
+console.log(lenght);
+console.log(lenght2);
